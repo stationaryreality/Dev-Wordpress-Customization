f3143d01265a5d56cdb06e76567e6c6c8193f15c
Stationary Reality <stationaryreality@tutanota.com>
2026-07-22 09:07:29 -0400

    removed overview from site resources section, added css for featured image on homepage grid

--- page-home.php
+++ page-home.php
@@ -175,49 +175,58 @@ get_header();
     ?>
 
     <?php
-    /*
-    |--------------------------------------------------------------------------
-    | 5. RESOURCES (Formerly "Site Resources", moved to bottom)
-    |--------------------------------------------------------------------------
-    */
-    $resource_ids = [];
-    foreach ($resource_slugs as $slug) {
-        $page = get_page_by_path($slug);
-        if ($page) $resource_ids[] = $page->ID;
-    }
+/*
+|--------------------------------------------------------------------------
+| 5. RESOURCES (Formerly "Site Resources", moved to bottom)
+|--------------------------------------------------------------------------
+*/
+$resource_ids = [];
+foreach ($resource_slugs as $slug) {
+    $page = get_page_by_path($slug);
+    if ($page) $resource_ids[] = $page->ID;
+}
 
-    if (!empty($resource_ids)) :
-    $resources_query = new WP_Query([
-        'post_type'      => 'page',
-        'posts_per_page' => -1,
-        'post__in'       => $resource_ids,
-        'orderby'        => 'post__in',
-    ]);
+// Exclude Platform Overview page from Resources section
+$overview_page = get_page_by_path('platform-overview');
+if ($overview_page) {
+    $exclude_ids = array_merge($resource_ids, [$overview_page->ID]);
+} else {
+    $exclude_ids = $resource_ids;
+}
 
-    if ($resources_query->have_posts()) :
-    ?>
-    <section class="homepage-section">
-        <h2 class="page-section-title">Resources</h2>
-        <div class="tag-posts-grid">
-            <?php while ($resources_query->have_posts()) : $resources_query->the_post(); ?>
-                <div class="tag-post-item">
-                    <a href="<?php the_permalink(); ?>" class="tag-post-thumbnail">
-                        <?php if (has_post_thumbnail()) : ?>
-                            <img src="<?php the_post_thumbnail_url('medium'); ?>" alt="<?php the_title(); ?>">
-                        <?php endif; ?>
-                    </a>
-                    <a href="<?php the_permalink(); ?>" class="tag-post-title">
-                        <?php the_title(); ?>
-                    </a>
-                </div>
-            <?php endwhile; ?>
-        </div>
-    </section>
-    <?php
-    endif;
-    wp_reset_postdata();
-    endif;
-    ?>
+if (!empty($resource_ids)) :
+$resources_query = new WP_Query([
+    'post_type'      => 'page',
+    'posts_per_page' => -1,
+    'post__in'       => $resource_ids,
+    'post__not_in'   => [$overview_page->ID ?? 0], // Explicitly exclude overview page
+    'orderby'        => 'post__in',
+]);
+
+if ($resources_query->have_posts()) :
+?>
+<section class="homepage-section">
+    <h2 class="page-section-title">Resources</h2>
+    <div class="tag-posts-grid">
+        <?php while ($resources_query->have_posts()) : $resources_query->the_post(); ?>
+            <div class="tag-post-item">
+                <a href="<?php the_permalink(); ?>" class="tag-post-thumbnail">
+                    <?php if (has_post_thumbnail()) : ?>
+                        <img src="<?php the_post_thumbnail_url('medium'); ?>" alt="<?php the_title(); ?>">
+                    <?php endif; ?>
+                </a>
+                <a href="<?php the_permalink(); ?>" class="tag-post-title">
+                    <?php the_title(); ?>
+                </a>
+            </div>
+        <?php endwhile; ?>
+    </div>
+</section>
+<?php
+endif;
+wp_reset_postdata();
+endif;
+?>
 
 </main>
 
