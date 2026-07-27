48b5b7ebe205b41c485d113cde0e4c717223d46b
Stationary Reality <stationaryreality@tutanota.com>
2026-07-27 10:53:49 -0400

    typos

--- functions.php
+++ functions.php
@@ -385,6 +385,6 @@ add_action('wp_enqueue_scripts', 'dev_site_enqueue_styles');
 // 2026-7-27
 
 
-require_once get_stylesheet_directory() . '/inc/record-breadcrumbs.php';
+require_once get_stylesheet_directory() . '/inc/helpers/record-breadcrumbs.php';
 
-require_once get_stylesheet_directory() . '/inc/record-redirects.php';
+require_once get_stylesheet_directory() . '/inc/helpers/record-redirects.php';
