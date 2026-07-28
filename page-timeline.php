c7d3207786a88f9a506f5d685e788193ad92a355
Stationary Reality <stationaryreality@tutanota.com>
2026-07-28 14:11:20 -0400

    added sql id to timeline

--- page-timeline.php
+++ page-timeline.php
@@ -50,6 +50,7 @@ wp_enqueue_style(
                     <th class="col-date">Date</th>
                     <th class="col-record">Record</th>
                     <th class="col-project">Project</th>
+                    <th class="col-sqlite">SQLite ID</th>
                     <th class="col-status">Status</th>
                 </tr>
             </thead>
@@ -104,6 +105,10 @@ wp_enqueue_style(
                         ?>
                     </td>
 
+                    <td class="col-sqlite">
+                            <?php echo esc_html(get_field('sqlite_id')); ?>
+                    </td>
+                    
                     <td class="col-status">
                         <span class="status-badge status-<?php echo esc_attr($review_status); ?>">
                             <?php 
